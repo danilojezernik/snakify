be962b503adb229665737defb9d5f4e4cbb80ac3
danilojezernik <dani.jezernik@gmail.com>
2023-03-05 22:41:16 +0100

    previous and next number in line

--- first/previous_next.py
+++ first/previous_next.py
@@ -4,3 +4,10 @@ See the examples below for the exact format your answers should take. There shou
 Remember that you can convert the numbers to strings using the function str.
 """
 
+number: int = int(input('Number: '))
+
+previous = number - 1
+next = number + 1
+
+print(f'The next number for the number {number} is {next}.')
+print(f'The next number for the number {number} is {previous}.')
