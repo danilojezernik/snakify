e6adfda9ad248a6df3ab02e89aec3cb4d69e7b8e
danilojezernik <dani.jezernik@gmail.com>
2023-03-06 10:47:49 +0100

    previous and next number in line

---
+++ first/two_timestamps.py
@@ -0,0 +1,17 @@
+"""
+A timestamp is three numbers: a number of hours, minutes and seconds.
+Given two timestamps, calculate how many seconds is between them.
+The moment of the first timestamp occurred before the moment of the second timestamp.
+"""
+
+h1: int = int(input('h1: '))
+m1: int = int(input('m1: '))
+s1: int = int(input('s1: '))
+
+h2: int = int(input('h2: '))
+m2: int = int(input('m2: '))
+s2: int = int(input('s2: '))
+
+seconds = (h2 - h1) * 3600 + (m2 - m1) * 60 + (s2 - s1)
+
+print(seconds)
