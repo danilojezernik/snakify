adc0072d242f0a7e2ef404d2a02bebace3719697
danilojezernik <dani.jezernik@gmail.com>
2023-03-07 09:46:51 +0100

    clock_face_two

---
+++ 2_integer_and_float_numbers/clock_face_two.py
@@ -0,0 +1,7 @@
+"""
+Hour hand turned by α degrees since the midnight.
+Determine the angle by which minute hand turned since the start of the current hour.
+Input and output in these problems are floating-point numbers.
+"""
+
+alfa: int = int(input('How many degrees, hour hand turned since midnight: '))
