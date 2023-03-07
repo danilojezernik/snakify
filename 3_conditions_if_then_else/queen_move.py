1ad1371cc8302cf1c1abd869c50f60e97bc38204
danilojezernik <dani.jezernik@gmail.com>
2023-03-07 16:42:14 +0100

    queen_move

---
+++ 3_conditions_if_then_else/queen_move.py
@@ -0,0 +1,18 @@
+"""
+Chess queen moves horizontally, vertically or diagonally to any number of cells.
+Given two different cells of the chessboard, determine whether a queen can go from the first cell to the second in one move.
+
+The program receives the input of four numbers from 1 to 8, each specifying the column and row number,
+first two - for the first cell, and then the last two - for the second cell.
+The program should output YES if a queen can go from the first cell to the second in one move, or NO otherwise.
+"""
+
+x1: int = int(input('x1: '))
+y1: int = int(input('y1: '))
+x2: int = int(input('x2: '))
+y2: int = int(input('y2: '))
+
+if x2 - x1 <= 1 and y2 - y1 <= 1 and (x1 - x2) == (y1 - y2):
+    print('YES')
+else:
+    print('NO')
