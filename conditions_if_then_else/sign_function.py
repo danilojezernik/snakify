dfa469f23a8c2ed7aa2412587727167cd9eb4036
danilojezernik <dani.jezernik@gmail.com>
2023-03-07 09:46:18 +0100

    sign_function

--- conditions_if_then_else/sign_function.py
+++ conditions_if_then_else/sign_function.py
@@ -3,6 +3,7 @@ For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if i
 Try to use the cascade if-elif-else for it.
 """
 
+
 number: int = int(input('Number: '))
 
 if number > 1:
